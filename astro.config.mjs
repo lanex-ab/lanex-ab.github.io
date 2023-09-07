6a1caf0551a02443bfb0c26771fa17a0ebe00593
Alexander Eklund <alexeklund460@gmail.com>
2023-09-07 14:11:00 +0200

    Change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lanex-ab.github.io",
-  base: "/",
   integrations: [
     starlight({
       title: "My Docs",
